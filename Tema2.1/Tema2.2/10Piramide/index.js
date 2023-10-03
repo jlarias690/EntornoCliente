d21b94b721a149d02cb9e597591f89fd6c9e4493
CEP-INFOR3-PC06\TECH <jarias7997@ceuandalucia.es>
2023-10-03 19:15:16 +0200

    no message

--- Tema2.1/Tema2.2/10Piramide/index.js
+++ Tema2.1/Tema2.2/10Piramide/index.js
@@ -5,10 +5,13 @@ do {
     alert("El valor introducido no es correcto introduce un numero positivo.");
   }
 } while (isNaN($num) || $num < 0);
-var vari1 = "*";
-var vari2 = "";
+var vari1 = $num;
+var vari2 = 1;
 for ($i = 0; $i < $num; $i++) {
-  vari2 = vari2 + vari1;
-
   document.write(vari2 + "<br>");
+  vari2 = vari2 + vari1;
+  vari1--;
+}
+for ($j = 0; $num > $i; $i++) {
+  document.write($j);
 }
