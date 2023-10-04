9b201665e70c162aafdcbd9f4773ebdd02a24f3b
CEP-INFOR3-PC06\TECH <jarias7997@ceuandalucia.es>
2023-10-04 18:02:47 +0200

    Hay que terminar el ejercicio 11 y 10

---
+++ Tema2.1/Tema2.2/11aplicacion/index.js
@@ -0,0 +1,16 @@
+var $nombreY = prompt("Escribe el nombre");
+var $apellido = prompt("Escribe tu apellido.");
+let $salario = Number(prompt("Salario"));
+var $edad = Number(prompt("¿EDAD?"));
+var $salarioDecimales = $salario.toFixed(2);
+//document.write($salarioDecimales);
+
+if (Number($salarioDecimales) >= Number(1000) || Number($salarioDecimales) <= Number(2000) {
+//  $porcentaje = $salarioDecimales * 0.03;
+//  $total = $salarioDecimales + $porcentaje;
+//  document.write(`Aumento del 3% ${$porcentaje}€ + ${$salarioDecimales} = ${$total.toFixed( 2)}`);
+} else if (
+  (Number($salarioDecimales) >= 1000 || Number($salarioDecimales) <= 2000) &&
+  $edad < 45
+) {
+}
