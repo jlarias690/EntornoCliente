782b8ea7af870a2c30d3b50aca11ae835827be6e
CEP-INFOR3-PC06\TECH <jarias7997@ceuandalucia.es>
2023-10-30 15:08:40 +0100

    Trabajo finalizado

--- Tema2/TrabajoEntregable/index.js
+++ Tema2/TrabajoEntregable/index.js
@@ -1,37 +1,59 @@
-posicion = 0;
-avance = 5;
+posicion1 = 0;
+posicion2 = 0;
 var $tem;
 
+//correr()->Empatan las dos figuras
 let correr = () => {
   imagen1 = document.getElementById("foto1");
   imagen2 = document.getElementById("foto2");
 
-  posicion += 3;
+  posicion1 += 1;
 
-  imagen1.style.left = posicion + "%";
-  imagen2.style.left = posicion + "%";
-  if (posicion >= 90) {
-    posicion = 0;
+  imagen1.style.left = posicion1 + "%";
+  imagen2.style.left = posicion1 + "%";
+  //Cada vez que llegue al noventa porciento de la pantalla se resetea a la posicion 0.
+  if (posicion1 >= 90) {
+    posicion1 = 0;
   }
+  document.getElementById("empate").disabled = true;
+  document.getElementById("gana").disabled = true;
 };
+//correrGanaEl2()-> La segunda figura gana.
+let correrGanaEl2 = () => {
+  imagen1 = document.getElementById("foto1");
+  imagen2 = document.getElementById("foto2");
 
+  posicion1 += 1;
+  posicion2 += 2;
+  imagen1.style.left = posicion1 + "%";
+  imagen2.style.left = posicion2 + "%";
+
+  if (posicion1 >= 90) {
+    posicion1 = 0;
+  }
+  if (posicion2 >= 90) {
+    posicion2 = 0;
+  }
+  document.getElementById("empate").disabled = true;
+  document.getElementById("gana").disabled = true;
+};
+// reinicio()-> Reiniciamos las posiciones a 0.
+let reinicio = () => {
+  posicion1 = 0;
+  posicion2 = 0;
+  imagen1.style.left = posicion1 + "%";
+  imagen2.style.left = posicion2 + "%";
+
+  //Habilitamos los botones que se desactivaron anteriormente,
+  document.getElementById("empate").disabled = false;
+  document.getElementById("gana").disabled = false;
+};
 let inicio = () => {
   $tem = window.setInterval(correr, 100);
+};
+let inicio2 = () => {
+  $tem = window.setInterval(correrGanaEl2, 100);
 };
 let stop = () => {
   clearInterval($tem);
 };
-
-/*
-var imagen = document.getElementById("foto1");
-
-var posicion = 0;
-
-function inicio() {
-  alert(imagen.style.left);
-  imagen.style.left = "300px";
- 
-  posicion += 10;
-  image.style.left = posicion + "px";
-}
-*/
