b2a7cc068b652d0df42646c898d5474b1c12fd0f
CEP-INFOR3-PC06\TECH <jarias7997@ceuandalucia.es>
2023-11-16 18:05:17 +0100

    me la pela

---
+++ Tema4/15/index.js
@@ -0,0 +1,36 @@
+const array;
+
+window.addEventListener("DOMContentLoaded", (e) => {
+   array = [
+    {
+      id: 1,
+      nombre: "pepe",
+      precio: 150,
+      codigo: "A1",
+    },
+    {
+      id: 2,
+      nombre: "PIPO",
+      precio: 100,
+      codigo: "A2",
+    },
+    {
+      id: 3,
+      nombre: "LIPO",
+      precio: 50,
+      codigo: "A3",
+    },
+    {
+      id: 4,
+      nombre: "ZIPO",
+      precio: 10,
+      codigo: "A4",
+    },
+  ];
+});
+
+array.array.forEach((array)) => {
+    cost tr = document.createElement('tr');
+    tr.setAt
+    
+};
